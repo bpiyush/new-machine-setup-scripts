23040141cee9b9e9a2ac5573250b51eca48575a2
Piyush (Mab M1) <piyushnbagad11@gmail.com>
2021-09-07 10:51:49 +0200

    Adds script to generate and add git creds/SSH keys

---
+++ set_git_creds.sh
@@ -0,0 +1,13 @@
+
+# set email and username
+git config --global user.name "Piyush Bagad"
+git config --global user.email "piyushnbagad11@gmail.com"
+
+# generate SSH key pair and copy the public key to be added to github
+eval `ssh-agent`
+ssh-add -l
+
+ssh-keygen -t rsa -b 4096 -C "piyushnbagad11@gmail.com"
+cat ~/.ssh/id_rsa.pub
+
+# after this, go to Settings > SSH & GPG keys > Add new key -> Add the key!
